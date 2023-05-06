8bbaf56b7156e35d5012d119ea413ec58d8a6b0e
diogooz-hub <112966199+diogooz-hub@users.noreply.github.com>
2023-05-06 09:14:59 +0100

    final cancel service

--- request_cancel_merytu/cancelByEmail.py
+++ request_cancel_merytu/cancelByEmail.py
@@ -1,11 +1,12 @@
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 #define endpoint
-url_post = "https://app.merytu.com/rest/nosecured/cancel"
+url_post = ""
 
 #get emails
-emailStr = "email1,email2,email3,email4,email5"
+emailStr = "joao.meryter@merytu.com,bernardo.pita@merytu.com"
 
 #string to list
 emailList = emailStr.split(sep=",")
@@ -13,12 +14,20 @@ print(f"email list from string received:{emailList}")
 
 
 #for emails in emailList:
+response_final_status = []
 for email in emailList:
     try:
         print(f"canceling: {email} now")
-        payload = {"api": "7924079d-200a-49a6-8d76-106b1ca8afc0", "type": "cancel", "email": {email}}
-        post_response = requests.post(url_post, json = payload)
+        payload = {"email":email, "type":"cancel", "api":"7924079d-200a-49a6-8d76-106b1ca8afc0"}
+        post_response = requests.post(url_post, json=payload)
         print(post_response.status_code)
         post_response.raise_for_status()
+        response_final_status.append(post_response.status_code)
     except requests.exceptions.RequestException as error:
         print(error)
+        response_final_status.append("fail")
+        
+if "fail" in response_final_status:
+    print("At least one fail")
+else:
+    print("Passed with great success, proud of you")
